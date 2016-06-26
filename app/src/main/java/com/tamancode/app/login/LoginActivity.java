ea6bd0b12fecd5df5c7996a9b0cb4e0464ead6cb
miftakhul <miftakhul.hudha@meruvian.org>
2016-06-26 12:33:14 +0700

    login screen

---
+++ app/src/main/java/com/tamancode/app/login/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.tamancode.app.login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tamancode.app.R;
+
+/**
+ * Created by miftakhul on 26/06/16.
+ */
+public class LoginActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
